18aacfd493ac13a04b95c9b4289bb6d47562b442
Fandorin <SherJlok15@gmail.com>
2019-06-11 12:41:31 +0300

    Updates

---
+++ precache-manifest.8b6f42ef6b6860485be61c04661a1197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e195b13a2b47275d1d345545b60b29",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "63ea2c2144a637565d7d",
+    "url": "/react_static-list-of-todos/static/css/main.a1ee0d9c.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "63ea2c2144a637565d7d",
+    "url": "/react_static-list-of-todos/static/js/main.e8d56e2e.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
